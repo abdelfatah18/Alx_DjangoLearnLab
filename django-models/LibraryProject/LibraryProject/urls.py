03826b014c72bea04c981465b5876d4c3039b676
abdelfatah18 <aaa5122003.com>
2024-12-31 22:08:33 +0200

    Resolved merge conflicts

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('',include('relationship_app.urls')),
+     path('pages/',include('relationship_app.urls')),
 ]
