04d128391619c6441bf12c18dc4d3a85ef9dc236
abdelfatah hany <aaa5122003@gmail.com>
2024-12-31 22:25:30 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,18 +6,13 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    # رابط التسجيل
+
     path('register/', views.register, name='register'),
-    
-    # رابط تسجيل الدخول
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    
-    # رابط تسجيل الخروج
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
-    
-    # يمكنك إضافة المزيد من الروابط هنا حسب احتياجك
     path('', views.home, name='home'),
 ]
+
 
 
 
