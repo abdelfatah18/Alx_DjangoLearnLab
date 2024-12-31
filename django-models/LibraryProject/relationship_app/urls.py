11e9328510fd3566870a0f4b3b983ca276b885ce
abdelfatah hany <aaa5122003@gmail.com>
2024-12-31 22:14:58 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,21 @@
 from django.urls import path
-from .views import list_books 
-from .views import LibraryDetailView
 from . import views
-
 from django.contrib.auth.views import LoginView, LogoutView
-
-from .views import CustomLoginView, CustomLogoutView, register
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    # رابط التسجيل
+    path('register/', views.register, name='register'),
+    
+    # رابط تسجيل الدخول
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    
+    # رابط تسجيل الخروج
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    
+    # يمكنك إضافة المزيد من الروابط هنا حسب احتياجك
     path('', views.home, name='home'),
 ]
 
