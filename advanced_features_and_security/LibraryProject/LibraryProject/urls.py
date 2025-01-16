7eb2a6bdf5e7fe2c0adc630cc766b5e7a54ad47f
abdelfatah hany <aaa5122003@gmail.com>
2025-01-16 22:44:50 +0200

    Delete advanced_features_and_security directory

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from relationship_app import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('pages/', include('relationship_app.urls')),
-    path('', views.home, name='home'), 
-]
