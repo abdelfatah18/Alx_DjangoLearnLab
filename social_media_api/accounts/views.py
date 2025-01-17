cb1a8c11d4e823b907ce222653ff70e9fd068af2
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 17:53:20 +0200

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,6 +3,18 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from accounts.models import CustomUser
 from accounts.serializers import UserSerializer
+from rest_framework import generics, permissions
+from posts.models import Post
+from posts.serializers import PostSerializer
+
+class UserFeedView(generics.ListAPIView):
+    """Displays posts from users that the current user follows."""
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve posts from followed users, ordered by newest first."""
+        return Post.objects.filter(author__in=self.request.user.following.all()).order_by('-created_at')
 
 class FollowUserView(generics.GenericAPIView):
     """Allows an authenticated user to follow another user."""
