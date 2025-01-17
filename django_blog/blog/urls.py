629f85e50e4109e7c0ef18e0e1a7704a21799a29
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 06:18:00 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,9 +6,4 @@ urlpatterns = [
     path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),  # Create new comment
     path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),  # Edit comment view
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),  # Delete comment view
-    path('', PostListView.as_view(), name='post-list'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # Updated path
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
