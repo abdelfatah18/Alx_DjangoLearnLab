93e7541bdac753f7f02f694acd02469aaafffe1d
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 18:40:48 +0200

    Update views.py

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from .models import Notification
 
-# Create your views here.
+@login_required
+def get_notifications(request):
+    notifications = Notification.objects.filter(recipient=request.user, read=False)
+    data = [{"actor": notification.actor.username, "verb": notification.verb, "timestamp": notification.timestamp} for notification in notifications]
+    return JsonResponse(data, safe=False)
