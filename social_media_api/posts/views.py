bf7218dc38407146c7d7c130141bef3cf54f247f
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 18:09:12 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,33 +1,13 @@
 from rest_framework import generics, permissions
 from posts.models import Post
 from posts.serializers import PostSerializer
-from django.shortcuts import get_object_or_404
-
-class PostListCreateView(generics.ListCreateAPIView):
-    """Allows users to create and list posts."""
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        """Associates the logged-in user as the author of the post."""
-        serializer.save(author=self.request.user)
-
-class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """Allows users to retrieve, update, or delete a post."""
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        """Ensure users can only edit or delete their own posts."""
-        return Post.objects.filter(author=self.request.user)
 
 class UserFeedView(generics.ListAPIView):
-    """Displays posts from users that the current user follows."""
+    """Returns posts from followed users, ordered by creation date."""
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        """Retrieve posts from followed users, ordered by newest first."""
-        return Post.objects.filter(author__in=self.request.user.following.all()).order_by('-created_at')
+        """Returns posts from users that the current user follows."""
+        following_users = self.request.user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
