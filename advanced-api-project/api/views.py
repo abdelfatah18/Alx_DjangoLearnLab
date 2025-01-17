37e62f245a44b5c036c4ccd3e3e0ea9c546a102d
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 02:22:16 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+r
 
 # عرض قائمة الكتب (عام للجميع)
 class BookListView(generics.ListAPIView):
@@ -30,3 +33,14 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
+
+
+# عرض قائمة الكتب (عام للجميع)
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
