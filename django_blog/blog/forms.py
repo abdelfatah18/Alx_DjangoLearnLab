25576f7bfd4ba85a49248b2adf69c53162efbe13
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 06:15:13 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Comment
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -9,3 +10,18 @@ class CommentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['content'].widget.attrs.update({'rows': 4, 'placeholder': 'Write your comment here...'})
+
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if not content:
+            raise forms.ValidationError("Content cannot be empty.")
+        if len(content) < 10:
+            raise forms.ValidationError("Comment must be at least 10 characters long.")
+        return content
