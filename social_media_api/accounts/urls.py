b373700bc119356d9125fc3692ed9efe920b45c2
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 18:16:59 +0200

    Delete social_media_api directory

--- social_media_api/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from accounts.views import FollowUserView, UnfollowUserView
-
-urlpatterns = [
-    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
-]
--- social_media_api/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from posts.views import PostListCreateView, PostDetailView, UserFeedView
-
-urlpatterns = [
-    path('posts/', PostListCreateView.as_view(), name='post-list-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('feed/', UserFeedView.as_view(), name='user-feed'),
-]
