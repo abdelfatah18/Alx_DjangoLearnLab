e9900f04baa447e4ac19e0d7b5e36c4aee07ea11
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 18:25:29 +0200

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,12 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', PostListCreateView.as_view(), name='post-list-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('feed/', UserFeedView.as_view(), name='user-feed'),     
+    
     path('posts/<int:pk>/like/', views.like_post, name='like_post'),
     path('posts/<int:pk>/unlike/', views.unlike_post, name='unlike_post'),
-    
 ]
-
-
