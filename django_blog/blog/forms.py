5998cf70f0337f9c2c5485f6baac40be1bf3a73e
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 06:34:46 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,37 +1,10 @@
+# blog/forms.py
 from django import forms
-from .models import Comment
-from .models import Post, Tag
-from taggit.forms import TagField  # If using django-taggit
-
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['content']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['content'].widget.attrs.update({'rows': 4, 'placeholder': 'Write your comment here...'})
-
-
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['content']
-
-    def clean_content(self):
-        content = self.cleaned_data.get('content')
-        if not content:
-            raise forms.ValidationError("Content cannot be empty.")
-        if len(content) < 10:
-            raise forms.ValidationError("Comment must be at least 10 characters long.")
-        return content
-
-
+from .models import Post
+from taggit.forms import TagField, TagWidget  # Import TagWidget and TagField
 
 class PostForm(forms.ModelForm):
-    tags = TagField()  # Allows adding tags using django-taggit
+    tags = TagField(widget=TagWidget())  # Use TagWidget for tagging functionality
 
     class Meta:
         model = Post
