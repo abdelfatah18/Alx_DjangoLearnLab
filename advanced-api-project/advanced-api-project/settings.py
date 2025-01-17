d3d6ae29c94140dad4b7100ff80cf22976c2ba56
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 02:30:01 +0200

    Update settings.py

--- advanced-api-project/advanced-api-project/settings.py
+++ advanced-api-project/advanced-api-project/settings.py
@@ -39,15 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
-    'django_filters',
+    'django_filters'
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
