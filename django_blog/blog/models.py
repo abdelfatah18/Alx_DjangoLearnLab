f27dffbd6cef47bdb4d91db42bcf1047e85a55da
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 06:25:35 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -27,3 +27,11 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+        
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+
+    def __str__(self):
+        return self.title
