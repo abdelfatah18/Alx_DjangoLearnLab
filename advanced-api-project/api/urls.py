48353f906ec1f1df8bb3fceaf5b6d18d72e6ffb6
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 02:10:07 +0200

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from .views import (
+    BookListView, BookDetailView, BookCreateView, 
+    BookUpdateView, BookDeleteView
+)
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
