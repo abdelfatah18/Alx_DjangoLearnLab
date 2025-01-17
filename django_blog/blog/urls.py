7148444591eda902224cfccc8e7538a82d118a0c
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 06:22:36 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,7 +3,7 @@ from .views import post_detail, CommentCreateView, CommentUpdateView, CommentDel
 
 urlpatterns = [
     path('post/<int:pk>/', post_detail, name='post-detail'),  # Post detail view
-    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),  # Correct path for new comment
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),  # Edit comment view
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),  # New comment view
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),  # Update comment view
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),  # Delete comment view
 ]
