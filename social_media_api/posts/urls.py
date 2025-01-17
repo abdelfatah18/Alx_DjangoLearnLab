b9356855140d688cd88b937f3dc3486bb78135a7
abdelfatah hany <aaa5122003@gmail.com>
2025-01-17 18:40:27 +0200

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/like/', views.like_post, name='like_post'),
-    path('<int:pk>/unlike/', views.unlike_post, name='unlike_post'),
+   path('<int:pk>/like/', views.like_post, name='like_post'),
+   path('<int:pk>/unlike/', views.unlike_post, name='unlike_post'),,
 ]
