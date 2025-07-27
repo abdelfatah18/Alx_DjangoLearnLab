20ca5718dcbbfbc139d3abc8c9e54b4f5403da18
abdelfatah hany <aaa5122003@gmail.com>
2025-07-27 23:52:42 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from .models import Book
 
 @permission_required('relationship_app.can_view', raise_exception=True)
-def list_books(request):
+def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
