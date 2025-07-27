47c09b4c489e76f10c6bb71ad910e8f054fe609f
abdelfatah hany <aaa5122003@gmail.com>
2025-07-27 23:50:08 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,14 +3,22 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class Book(models.Model):
-    
-    title=models.CharField(max_length=200)
-    author=models.CharField(max_length=100)
-    publication_year=models.IntegerField()
-    
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
     def __str__(self):
-        return f"{self.title}"
+        return self.title
+
 
 # accounts/models.py
 
