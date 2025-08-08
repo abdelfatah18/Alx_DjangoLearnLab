e5ff83537ea76abe8097ebd16eacbbc4aa721e6b
abdelfatah hany <aaa5122003@gmail.com>
2025-08-08 10:58:31 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 from datetime import datetime
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 # ✅ List all books (Public)
 class BookListView(generics.ListAPIView):
