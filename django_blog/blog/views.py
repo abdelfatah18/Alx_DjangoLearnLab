2c6b151561eb05a174abba0d694f5eb2889aeefb
abdelfatah hany <aaa5122003@gmail.com>
2025-08-13 11:27:33 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -7,6 +7,36 @@ from .models import Post, Comment
 from .forms import CommentForm
 
 
+from django.contrib.auth import views as auth_views
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django import forms
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = RegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    return render(request, "blog/profile.html")
+
 # Post Detail with comments
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
