d16453cc3c7e308d9559e475a0e8e4acaef2b8b2
abdelfatah hany <aaa5122003@gmail.com>
2025-08-13 11:34:55 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,6 +9,7 @@ from .views import (
     PostUpdateView, PostDeleteView
 )
 
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
@@ -27,7 +28,7 @@ urlpatterns = [
     path('posts/', PostListView.as_view(), name='post_list'),
     path('posts/new/', PostCreateView.as_view(), name='post_create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
 
