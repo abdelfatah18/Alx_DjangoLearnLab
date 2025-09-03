2f52f35d0e6138a05b522e5af53226ea9bb46d15
abdelfatah hany <aaa5122003@gmail.com>
2025-09-03 18:37:02 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,41 +1,30 @@
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.decorators import login_required
-from rest_framework import generics, permissions
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from .models import Post, Like
-from notifications.models import Notification
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
-@api_view(['POST'])
-@permission_classes([permissions.IsAuthenticated])
-def like_post(request, pk):
-    post = generics.get_object_or_404(Post, pk=pk)
-    like, created = Like.objects.get_or_create(user=request.user, post=post)
-    if created:
-        Notification.objects.create(
-            recipient=post.user,
-            actor=request.user,
-            verb="liked your post",
-            target=post
-        )
-        return Response({'message': 'Post liked successfully.'}, status=200)
-    else:
-        return Response({'message': 'You have already liked this post.'}, status=400)
 
-@api_view(['POST'])
-@permission_classes([permissions.IsAuthenticated])
-def unlike_post(request, pk):
-    post = generics.get_object_or_404(Post, pk=pk)
-    like = Like.objects.filter(post=post, user=request.user)
-    if like.exists():
-        like.delete()
-        Notification.objects.create(
-            recipient=post.user,
-            actor=request.user,
-            verb="unliked your post",
-            target=post
-        )
-        return Response({'message': 'Post unliked successfully.'}, status=200)
-    else:
-        return Response({'message': 'You have not liked this post.'}, status=400)
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit/delete it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user or request.method in permissions.SAFE_METHODS
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by("-created_at")
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
